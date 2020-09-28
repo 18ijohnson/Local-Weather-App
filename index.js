ac5fd0f5f21d80ebab147bd35df8c2349d66ef76
Isaac Johnson <droonspy@gmail.com>
2020-09-28 12:41:38 -0700

    added error if location cannot be accessed

--- index.js
+++ index.js
@@ -10,54 +10,52 @@ let unit = 'f';
 let temperatureC = '';
 let temperatureF = '';
 
-function getWeather() {
-  //Get location and then get weather from freeCodeCamp.org weather api
-  navigator.geolocation.getCurrentPosition(position => {
-    fetch(`https://fcc-weather-api.freecodecamp.repl.co/api/current?lat=${position.coords.latitude}&lon=${position.coords.longitude}`)
-      .then(response => response.json()).then(json => {
-        //Set data on elements
-        statusP.textContent = json.weather[0].description.substr(0, 1).toUpperCase() + json.weather[0].description.substr(1)
-        locationP.textContent = json.name
-        temperatureC = Math.floor(json.main.temp) + '°'
-        temperatureF = Math.floor((json.main.temp * 9 / 5) + 32) + '°'
-        tempP.textContent = (unit == 'f') ? temperatureF : temperatureC
+function getWeather(position) {
+  //Get weather from freeCodeCamp.org weather api
+  fetch(`https://fcc-weather-api.freecodecamp.repl.co/api/current?lat=${position.coords.latitude}&lon=${position.coords.longitude}`)
+    .then(response => response.json()).then(json => {
+      //Set data on elements
+      statusP.textContent = json.weather[0].description.substr(0, 1).toUpperCase() + json.weather[0].description.substr(1)
+      locationP.textContent = json.name
+      temperatureC = Math.floor(json.main.temp) + '°'
+      temperatureF = Math.floor((json.main.temp * 9 / 5) + 32) + '°'
+      tempP.textContent = (unit == 'f') ? temperatureF : temperatureC
 
-        //Set weather icon based on api weather category
-        let FAIcon = '';
-        switch (json.weather[0].main) {
-          case 'Rain':
-            FAIcon = 'cloud-showers-heavy'
-            break;
-          case 'Drizzle':
-            FAIcon = 'cloud-sun-rain'
-            break;
-          case 'Clouds':
-            FAIcon = 'cloud'
-            break;
-          case 'Snow':
-            FAIcon = 'snowflake'
-            break;
-          case 'Clear':
-            FAIcon = 'sun'
-            break;
-          case 'Thunderstorm':
-            FAIcon = 'bolt'
-            break;
-          case 'Mist':
-          case 'Smoke':
-          case 'Haze':
-          case 'Dust':
-          case 'Fog':
-          case 'Sand':
-          case 'Ash':
-          case 'Squall':
-          case 'Tornado':
-            FAIcon = 'smog'
-            break;
-        }
-        icon.className = `fas fa-${FAIcon} fa-8x`
-      })
-  })
+      //Set weather icon based on api weather category
+      let FAIcon = '';
+      switch (json.weather[0].main) {
+        case 'Rain':
+          FAIcon = 'cloud-showers-heavy'
+          break;
+        case 'Drizzle':
+          FAIcon = 'cloud-sun-rain'
+          break;
+        case 'Clouds':
+          FAIcon = 'cloud'
+          break;
+        case 'Snow':
+          FAIcon = 'snowflake'
+          break;
+        case 'Clear':
+          FAIcon = 'sun'
+          break;
+        case 'Thunderstorm':
+          FAIcon = 'bolt'
+          break;
+        case 'Mist':
+        case 'Smoke':
+        case 'Haze':
+        case 'Dust':
+        case 'Fog':
+        case 'Sand':
+        case 'Ash':
+        case 'Squall':
+        case 'Tornado':
+          FAIcon = 'smog'
+          break;
+      }
+      icon.className = `fas fa-${FAIcon} fa-8x`
+    })
 }
 
 //Toggle temperature units between C and F when toggle is pressed.
@@ -66,6 +64,7 @@ function toggleCF() {
   tempP.textContent = (unit == 'f') ? temperatureF : temperatureC
 }
 
-//Initial setup and 30s repeat
-getWeather()
-setInterval(getWeather, 30000)
+navigator.geolocation.getCurrentPosition((position) => getWeather(position), (err) => {
+  alert('ERROR! Please ensure your browser supports geolocation access and that it is enabled for this site.')
+  console.error(err)
+})
